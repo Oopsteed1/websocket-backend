a451ebbe547b30eca68987b9e5e0ec2c22518f27
Steed GL <steed@webglsoft.com>
2023-11-09 00:02:06 +0800

    ver.03

--- src/server.ts
+++ src/server.ts
@@ -3,33 +3,25 @@ import express from 'express';
 
 const cors = require('cors');
 const app = express();
+const votedNames: { [key: string]: boolean } = {};
+let voteResults: { [key: string]: { [key: string]: number } } = {};
+let namesForDraw: string[] = [];
+let playerChoices: { name: string, choice: string }[] = [];
+let currentVoteTopic: string | null = null;
+
+app.use(cors());
 const server = app.listen(3000, () => {
   console.log('WebSocket server is running on port 3000');
 });
 const wss = new WebSocket.Server({ server });
-const voteResults: { [key: string]: { [key: string]: number } } = {};
-const votedNames: { [key: string]: boolean } = {};
-const namesForDraw: string[] = [];
-// 存储玩家的选择
-const playerChoices: { [key: string]: string } = {};
-let currentVoteTopic: string | null = null;
 
-app.use(cors());
-
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    if (typeof message === 'string') {
-      const messageString: string = message as string;
-      console.log(`Received: ${messageString}`);
-      ws.send(`You sent: ${messageString}`);
-    } else if (message instanceof Buffer) {
-      const messageBuffer: Buffer = message as Buffer;
-      console.log('Received binary data');
-    } else {
-      console.log('Received message of unknown type');
-    }
-  });
-});
+wss.on('connection', ws => {
+  console.log('[Client connected]')
+  // Connection closed
+  ws.on('close', () => {
+      console.log('Close connected')
+  })
+})
 
 // 先判斷要怎樣投票的API (贊成反對 / 剪刀石頭布 / 抽籤)
 app.get('/api/start-vote', (req, res) => {
@@ -42,117 +34,173 @@ app.get('/api/start-vote', (req, res) => {
       }
       currentVoteTopic = topic;
     }
-    res.json({ message: `Voting started for AgreeOrDisagree with topic: ${topic}.` });
+    res.json({ message: `投票主題 : ${topic}` });
   } else if (voteType === 'rockPaperScissors') {
     currentVoteTopic = topic;
-    res.json({ message: `Voting started for RockPaperScissors with topic: ${topic}.` });
+    res.json({ message: `投票主題 : ${topic}` });
   } else if (voteType === 'lottery') {
     currentVoteTopic = topic;
-    res.json({ message: `Voting started for Lottery with topic: ${topic}.` });
+    res.json({ message: `投票主題 : ${topic}` });
   } else {
-    res.status(400).json({ message: 'Invalid vote type.' });
+    res.status(400).json({ message: '無效的投票方式' });
   }
 });
 
-
-// 贊成反對+剪刀石頭布的API (有總數的那種)
+// --------------------------------贊成反對API 開始---------------------------- //
+// 贊成反對的API (有總數的那種)
 app.post('/api/submit-vote', express.json(), (req, res) => {
   const name = req.body.name;
-  const choice = req.body.choice; // 从请求体中获取用户的选择
+  const choice = req.body.choice;
+  if (choice !== 'agree' && choice !== 'disagree') {
+    return res.status(400).json({ message: 'Invalid choice. Must be "agree" or "disagree".' });
+  }
   if (votedNames[name]) {
-    res.status(400).json({ message: 'You have already voted.' });
-    return;
+    return res.status(400).json({ message: 'You have already voted.' });
   }
   if (name in voteResults) {
     voteResults[name][choice]++;
   } else {
-    voteResults[name] = { rock: 0, paper: 0, scissors: 0 };
+    voteResults[name] = { agree: 0, disagree: 0 };
     voteResults[name][choice] = 1;
   }
   votedNames[name] = true;
   res.json({ message: 'Vote submitted.' });
 });
 
-// 剪刀石頭布api
-app.post('/api/rockPaperScissors', express.json(), (req, res) => {
-  const players = req.body.players;  // 修正此处的解构
-
-  if (!players || !Array.isArray(players)) {
-    return res.status(400).json({ message: 'Invalid or missing players data.' });
+// 結束贊成反對 計算總數 且給結果
+app.get('/api/end-vote', (req, res) => {
+  const agreeOrDisagreeResults = calculateVotesByType(voteResults, ['agree', 'disagree']);
+  const agreeCount = agreeOrDisagreeResults['agree'];
+  const disagreeCount = agreeOrDisagreeResults['disagree'];
+  let winner = 'draw';
+  if (agreeCount > disagreeCount) {
+    winner = 'agree';
+  } else if (disagreeCount > agreeCount) {
+    winner = 'disagree';
   }
-
-  // 处理每个玩家的选择
-  players.forEach((player) => {
-    const { name, choice } = player;
-    if (!name || !choice) {
-      return res.status(400).json({ message: 'Both player name and choice are required.' });
-    }
-    playerChoices[name] = choice;
-  });
-  res.json({ message: 'Choices received from players.' });
-});
-
-
-// 计算剪刀石头布结果的API
-app.get('/api/rockPaperScissors/result', (req, res) => {
-  const playerNames = Object.keys(playerChoices);
-
-  if (playerNames.length < 2) {
-    return res.status(400).json({ message: 'Need at least two players for the game.' });
-  }
-
-  let result = "It's a tie!";
-  const winningRules: {
-    [choice: string]: string;
-  } = {
-    rock: 'scissors',
-    scissors: 'paper',
-    paper: 'rock',
+  const voteResponse = {
+    total: {
+      results: {
+        agree: agreeCount,
+        disagree: disagreeCount
+      },
+    },
+    winner: winner
   };
-
-  const results: { [name: string]: string } = {};
-
-  for (let i = 0; i < playerNames.length; i++) {
-    results[playerNames[i]] = 'draw';
-    for (let j = 0; j < playerNames.length; j++) {
-      if (i !== j) {
-        const player1Choice = playerChoices[playerNames[i]];
-        const player2Choice = playerChoices[playerNames[j]];
-        if (player1Choice === player2Choice) {
-          results[playerNames[i]] = 'draw';
-        } else if (winningRules[player1Choice] === player2Choice) {
-          if (results[playerNames[i]] !== 'draw') {
-            results[playerNames[i]] = 'draw';
-          } else {
-            results[playerNames[i]] = playerNames[j];
-          }
-        }
-      }
-    }
-  }
-
-  res.json({ results });
+  res.json(voteResponse);
 });
 
 
+function calculateVotesByType(results: { [key: string]: { [key: string]: number } }, types: string[]) {
+  const typeResults: { [key: string]: number } = {};
+  for (const key of Object.keys(results)) {
+    for (const type of types) {
+      typeResults[type] = (typeResults[type] || 0) + (results[key][type] || 0);
+    }
+  }
+  return typeResults;
+}
 
-// 儲存抽籤名字的api 
+// 重製贊成反對的API
+app.post('/api/reset-vote', (req, res) => {
+  for (const key of Object.keys(voteResults)) {
+    voteResults[key]['agree'] = 0;
+    voteResults[key]['disagree'] = 0;
+  }
+  res.json({ message: 'choices have been reset.' });
+});
+// --------------------------------贊成反對API 結束---------------------------- //
+
+
+
+// --------------------------------剪刀石頭布API 開始-------------------------- //
+
+// 儲存玩家选择的API
+app.post('/api/rockPaperScissors', express.json(), (req, res) => {
+  const players = req.body.players;
+  if (!players || !Array.isArray(players) || players.length !== 1) {
+    return res.status(400).json({ message: 'Exactly one player at a time can make a choice.' });
+  }
+  const player = players[0];
+  const { name, choice } = player;
+  if (!name || !choice) {
+    return res.status(400).json({ message: 'Both player name and choice are required.' });
+  }
+  if (playerChoices.length >= 2) {
+    return res.status(400).json({ message: 'Cannot store more than two players.' });
+  }
+  if (playerChoices.some(existingPlayer => existingPlayer.name === name)) {
+    return res.status(400).json({ message: 'Player name already exists in the list.' });
+  }
+  playerChoices.push(player);
+  res.json({ message: 'Choice received from the player.' });
+});
+
+
+// 查詢剪刀石頭布的名單API
+app.get('/api/rockPaperScissors/getPlayers', (req, res) => {
+  // 在这里返回名单数据
+  res.json({ players: playerChoices });
+});
+
+
+// 获取剪刀石头布比较结果的API
+app.get('/api/rockPaperScissors/result', (req, res) => {
+  if (playerChoices.length !== 2) {
+    return res.status(400).json({ message: 'Need exactly two players for comparison.' });
+  }
+
+  const player1Choice = playerChoices[0].choice;
+  const player2Choice = playerChoices[1].choice;
+  let finalResult = '平手';
+
+  if (player1Choice === player2Choice) {
+    finalResult = '平手';
+  } else if (player1Choice === 'rock' && player2Choice === 'scissors') {
+    finalResult = `贏家: ${playerChoices[0].name}`;
+  } else if (player1Choice === 'rock' && player2Choice === 'paper') {
+    finalResult = `贏家: ${playerChoices[1].name}`;
+  } else if (player1Choice === 'scissors' && player2Choice === 'paper') {
+    finalResult = `贏家: ${playerChoices[0].name}`;
+  } else if (player1Choice === 'scissors' && player2Choice === 'rock') {
+    finalResult = `贏家: ${playerChoices[1].name}`;
+  } else if (player1Choice === 'paper' && player2Choice === 'rock') {
+    finalResult = `贏家: ${playerChoices[0].name}`;
+  } else if (player1Choice === 'paper' && player2Choice === 'scissors') {
+    finalResult = `贏家: ${playerChoices[1].name}`;
+  }
+  res.json({ finalResult });
+});
+
+// 清除玩家选择的API
+app.post('/api/rockPaperScissors/reset', (req, res) => {
+  playerChoices = [];
+  res.json({ message: 'Player choices have been reset.' });
+});
+// --------------------------------剪刀石頭布API 結束-------------------------- //
+
+
+
+// --------------------------------抽籤API 開始------------------------------- //
+
+// 儲存抽籤名字的api
 app.post('/api/submit-name-for-lottery', express.json(), (req, res) => {
   const name = req.body.name;
   if (!name) {
     return res.status(400).json({ message: 'Name is required.' });
+  }
+  if (namesForDraw.includes(name)) {
+    return res.status(400).json({ message: 'Name already exists in the lottery.' });
   }
   namesForDraw.push(name);
   res.json({ message: 'Name submitted for the lottery.' });
 });
 
 
-
 // 查詢抽籤名單的api
 app.get('/api/get-lottery-list', (req, res) => {
   res.json({ names: namesForDraw });
 });
-
 
 
 // 抽籤的api + 結果
@@ -166,27 +214,10 @@ app.get('/api/draw-lottery', (req, res) => {
   res.json({ winner });
 });
 
-
-// 結束投票 計算總數
-app.get('/api/end-vote', (req, res) => {
-  const agreeOrDisagreeResults = calculateVotesByType(voteResults, ['agree', 'disagree']);
-  const rockPaperScissorsResults = calculateVotesByType(voteResults, ['rock', 'paper', 'scissors']);
-  const response = {
-    total: {
-      agreeOrDisagree: agreeOrDisagreeResults,
-      rockPaperScissors: rockPaperScissorsResults
-    }
-  };
-  res.json(response);
+// 重置抽奖名单的API
+app.post('/api/reset-lottery-list', (req, res) => {
+  namesForDraw = [];
+  res.json({ message: 'Lottery list has been reset.' });
 });
 
-function calculateVotesByType(results: { [key: string]: { [key: string]: number } }, types: string[]) {
-  const typeResults: { [key: string]: number } = {};
-  for (const key of Object.keys(results)) {
-    for (const type of types) {
-      typeResults[type] = (typeResults[type] || 0) + (results[key][type] || 0);
-    }
-  }
-  return typeResults;
-}
-
+// --------------------------------抽籤API 結束------------------------------- //
